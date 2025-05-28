f6cb31c6f2b33be508789ecf3b107e21c13a4b7f
toikaz <zxcursech@gmail.com>
2025-05-28 21:11:28 +0300

    Main file (Start VM)

--- Main.bat
+++ Main.bat
@@ -1,9 +1,16 @@
 @echo off
+echo =======================
+echo AEROASM-Virtual-machine
+echo =======================
+echo build 1.0 Alpha
+echo.
+echo.
 echo Enter AeroAsm file name without .aeroasm:
 set /p input=
 cls
+timeout 1 >nul
 echo Run program boot\%input%...
-timeout 1>nul
+timeout 1 >nul
 cls
 call VM.exe %input%
 pause
